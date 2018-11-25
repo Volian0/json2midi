323113051cb3f0b5c2c8a9fa133303d9ca33da9e
Volian0 <40314913+Volian0@users.noreply.github.com>
2018-11-25 15:22:08 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[])
 {
+    bool prompt = true;
     std::vector<std::string> args;
     for (int i = 1; i < argc; ++i)
         args.push_back(argv[i]);
@@ -10,8 +11,23 @@ int main(int argc, char* argv[])
     {
         std::cout << "Please specify JSON file!\nUsage example:\njson2midi <json> [auto]\njson2midi <json> [bpm1] [bb1] [bpm2] [bb2] [bpm3] [bb3]" << std::endl;
         std::cin.get();
+        return -1;
     }
-    else
+    if (args[0]=="-f")
+    {
+        prompt = false;
+        args.erase(args.begin());
+    }
+    try
+    {
         pt2::song song(args);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Exception:\n" << e.what() << std::endl;
+        if (prompt)
+            std::cin.get();
+        return -1;
+    }
     return 0;
 }
