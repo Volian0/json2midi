4025c642f5a2f47fbd6f53fd3114915e24d48417
Volian0 <40314913+Volian0@users.noreply.github.com>
2018-12-02 16:00:33 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
-#include "pt2.h"
-#include <iostream>
+#include "song_handler.h"
 
 int main(int argc, char* argv[])
 {
     std::vector<std::string> args;
     for (int i = 1; i < argc; ++i)
         args.push_back(argv[i]);
-    try
-    {
-        std::cout << args.at(0) << std::endl;
-        pt2::song song(args);
-        return 0;
-    }
-    catch (const std::exception& e)
-    {
-        std::cout << "Exception:\n" << e.what() << std::endl;
-    }
-    std::cin.get();
-    return -1;
+    return pt2::handle_file(args);
 }
